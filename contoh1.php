b969135ea0ba9e9b7244565e4ae8d4c439c71bc7
ArnitaA-0866 <102011561+ArnitaA-0866@users.noreply.github.com>
2022-03-21 16:51:31 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller{
+    public function index()
+    {
+        echo"<h1>HI</h1>";
+        echo"Nama : Arnita Aprida
+        NIM : 17190866
+        Kelas : 17.6E.01";
+        }
+}
