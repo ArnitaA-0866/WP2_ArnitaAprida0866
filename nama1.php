f45ad479db7bdb9ad2a4fee267f435eb6f496b0e
ArnitaA-0866 <102011561+ArnitaA-0866@users.noreply.github.com>
2022-05-21 18:09:40 +0700

    Add files via upload

---
+++ nama1.php
@@ -0,0 +1,10 @@
+<?php
+class nama1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>hai kenalin</h1>";
+        echo "Saya arnita <br>
+        kesukaan saya ialah tidur";
+    }
+}
---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Lengkap : Arnita Aprida</li>
+        <li>Username Instagram : Aprd.arn</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+<li>Jl. kampung sawah indah, bojonggede</li>
+</ul>
+
+<h4>Tempat/ttl</h4>
+<ul type="none">
+<li>Jakarta/11 april 2001</li>
+</ul>
+
+<h4>Hobby</h4>
+<ul type="square">
+<li>Taking a nap</li>
+</ul>
+
+</section>
